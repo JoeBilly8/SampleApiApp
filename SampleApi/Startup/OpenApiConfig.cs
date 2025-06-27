a3a282c224725393275b39d3abe645060541668f
Joe Moore <joebilly8@hotmail.co.uk>
2025-06-27 11:49:42 +0100

    restructure startup files

---
+++ SampleApi/Startup/OpenApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using Scalar.AspNetCore;
+
+namespace SampleApi.Startup
+{
+    static class OpenApiConfig
+    {
+        public static void AddOpenApiServices(this IServiceCollection services)
+        {
+            services.AddOpenApi();
+        }
+
+        public static void UseOpenApi(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference(options =>
+                {
+                    options.Title = "JM Sample API";
+                    options.Theme = ScalarTheme.Laserwave;
+                    options.Layout = ScalarLayout.Modern;
+                    options.HideClientButton = true;
+                });
+            }
+        }
+    }
+}
